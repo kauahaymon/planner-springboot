d6e439a643d92b1e99720071971b7f4cccabde7c
Kauã Haymon <kauahaymon@hotmail.com>
2024-07-28 15:23:36 -0400

    'MethodNotSupported' exception handling

--- src/main/java/com/rocketseat/planner/exception/GlobalExceptionHandler.java
+++ src/main/java/com/rocketseat/planner/exception/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.rocketseat.planner.exception;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
@@ -57,6 +58,22 @@ public class GlobalExceptionHandler {
                 status,
                 error,
                 e.getMessage(),
+                request.getRequestURI()
+        );
+        return ResponseEntity.status(status).body(response);
+    }
+
+    // Http Request Not Supported
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public ResponseEntity<StandardErrorResponse> handleHttpRequestMethodNotSupported(HttpRequestMethodNotSupportedException e, HttpServletRequest request) {
+        int status = HttpStatus.METHOD_NOT_ALLOWED.value();
+        String error = "Method Not Allowed";
+        String message = String.format("Request method '%s' not supported", e.getMethod());
+        StandardErrorResponse response = new StandardErrorResponse(
+                Instant.now(),
+                status,
+                error,
+                message,
                 request.getRequestURI()
         );
         return ResponseEntity.status(status).body(response);
